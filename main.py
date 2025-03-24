916731e5a42210f75c17c3e83b96e028dba15194
eltonaguiar77 <129380874+eltonaguiar77@users.noreply.github.com>
2025-03-24 12:38:22 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,6 @@ async def helloWorld():
     return {"message": "Hello World"}
 
 
-@app.get("/teste1")
-async def funcaoteste1():
-    return {"Teste": "Deu certo!"}
-
-@app.get("/teste2")
-async def funcaoteste2():
-    return {"Teste": "Deu certo de novo!"}
-
-@app.get("/teste3")
-async def funcaoteste3():
-    return {"Teste": True, "num_aleatorio:" : random.randint(0, 1000)}
+@app.get("/funcaoteste")
+async def funcaoteste():
+    return {"teste": True, "num aleatorio": random.randint(0, 20000)}
